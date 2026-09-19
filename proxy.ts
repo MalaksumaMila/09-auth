5600b19eae54948e92302b6012460151d121a13c
Mila <malaksumaaa@gmail.com>
2026-09-19 22:35:16 +0200

    update

--- proxy.ts
+++ proxy.ts
@@ -33,7 +33,7 @@ export async function proxy(request: NextRequest) {
         }
 
         if (isPublicRoute) {
-          return NextResponse.redirect(new URL('/profile', request.url), {
+          return NextResponse.redirect(new URL('/', request.url), {
             headers: {
               Cookie: cookieStore.toString(),
             },
